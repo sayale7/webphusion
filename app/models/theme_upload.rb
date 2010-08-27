b1922efc098a9348bddc99f93a1aeb8c5dcbc80d
Thomas Kohler <thomas.kohler@gmx.at>
2010-08-27 10:47:46 +0200

    neu

--- app/models/theme_upload.rb
+++ app/models/theme_upload.rb
@@ -2,11 +2,11 @@ class ThemeUpload < ActiveRecord::Base
 	
 	attr_accessible :theme_id, :theme_file
 	belongs_to :theme
-	
+	debugger
 	has_attached_file :theme_file,
-										# :url  => "/system/:class/:attachment/#{Theme.current_theme}/:basename.:extension",
-										# :path => ":rails_root/public/system/:class/:attachment/#{Theme.current_theme}/:basename.:extension"
-										:url  => "/system/:class/:attachment/53/:basename.:extension",
-										:path => ":rails_root/public/system/:class/:attachment/53/:basename.:extension"
+										:url  => "/system/:class/:attachment/#{Thread.current[:current_theme]}/:basename.:extension",
+										:path => ":rails_root/public/system/:class/:attachment/#{Thread.current[:current_theme]}/:basename.:extension"
+										# :url  => "/system/:class/:attachment/53/:basename.:extension",
+										# :path => ":rails_root/public/system/:class/:attachment/53/:basename.:extension"
 																										
 end
