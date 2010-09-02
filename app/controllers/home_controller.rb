256a36186ccdd2ec89aa77fd4c122409bcad63c3
Thomas Kohler <thomas.kohler@gmx.at>
2010-09-02 11:03:22 +0200

    page gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,22 @@
 class HomeController < ApplicationController
-  protect_from_forgery
-  layout 'application'
+	protect_from_forgery
+	layout 'application'
 
 	def index
 		unless request.url.include?('webphusion.com')
-			redirect_to "http://#{request.domain}/pages/86"
+			user = User.find_by_domain(request.domain.to_s).id
+			page = ""
+			unless user.nil?
+				website = Website.find_by_user_id(user.id)
+				unless website.nil?
+					page = Page.current_page = website.start_page_id
+				end
+			end
+			unless page.to_s.eql?('')
+				redirect_to "http://#{request.domain}/pages/#{page}"
+			else
+				redirect_to "http://webphusion.com"
+			end
 		end
 	end
 end
