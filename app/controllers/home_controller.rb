d695fb5f31eb4fe5b1238610a7450491f6d68479
Thomas Kohler <thomas.kohler@gmx.at>
2010-09-02 11:10:56 +0200

    page gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
 		unless request.url.include?('webphusion.com')
 			user = User.find_by_domain(request.domain.to_s).id
 			unless user.nil?
-				redirect_to "http://#{request.domain}/pages/#{Website.find_by_user_id(user.id).id}"
+				redirect_to "http://#{request.domain}/pages/#{Website.find_by_user_id(user.id).start_page_id}"
 				#website = Website.find_by_user_id(user.id)
 				# unless website.nil?
 				# 					redirect_to "http://#{request.domain}/pages/#{website.start_page_id}"
