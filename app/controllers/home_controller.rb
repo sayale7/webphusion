1261be153eaf2e934a0e0d0c98e8f255c3a70684
Thomas Kohler <thomas.kohler@gmx.at>
2010-09-02 11:07:43 +0200

    page gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,17 +5,11 @@ class HomeController < ApplicationController
 	def index
 		unless request.url.include?('webphusion.com')
 			user = User.find_by_domain(request.domain.to_s).id
-			page = ""
 			unless user.nil?
 				website = Website.find_by_user_id(user.id)
 				unless website.nil?
-					page = Page.current_page = website.start_page_id
+					redirect_to "http://#{request.domain}/pages/#{website.start_page_id}"
 				end
-			end
-			unless page.to_s.eql?('')
-				redirect_to "http://#{request.domain}/pages/#{page}"
-			else
-				redirect_to "http://webphusion.com?#{user}"
 			end
 		end
 	end
