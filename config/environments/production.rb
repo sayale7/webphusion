335e24578f75bc70a2210abcbb14159f5dbad839
Thomas Kohler <thomas.kohler@gmx.at>
2010-09-24 18:03:30 +0200

    newsletter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,4 +43,12 @@ Easywebman::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+
+	config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.delivery_method = :sendmail
+	config.action_mailer.sendmail_settings = {
+	  :location       => '/usr/sbin/sendmail',
+	  :arguments      => '-i -t -f support@yourapp.com'
+	}
+
 end
