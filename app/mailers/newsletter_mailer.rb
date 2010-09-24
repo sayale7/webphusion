7d78b15b618719217a0d4dfb002f7602a6f41b9f
Thomas Kohler <thomas.kohler@gmx.at>
2010-09-24 19:29:46 +0200

    newsletter

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,9 +1,8 @@
 class NewsletterMailer < ActionMailer::Base
   def newsletter(recipient, page)
-	    @recipient = recipient
-	    @url  = "http://example.com/login"
 			@page = page
 	    mail(:from => User.find(page.website_id).email,
+	 				  # headers["Reply-to"] =  User.find(page.website_id).email,
 					 :to => recipient.email,
 	         :subject => page.name)
 	 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,11 +44,13 @@ Easywebman::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-	config.action_mailer.raise_delivery_errors = false
 	config.action_mailer.delivery_method = :sendmail
+	# Defaults to:
 	config.action_mailer.sendmail_settings = {
-	  :location       => '/usr/sbin/sendmail',
-	  :arguments      => '-i -t -f support@yourapp.com'
+	  :location => '/usr/sbin/sendmail',
+	  :arguments => '-i -t'
 	}
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.raise_delivery_errors = true
 
 end
