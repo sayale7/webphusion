b08abb35aee5f5a1e5bf3e96bb6297111311ed50
Thomas Kohler <thomas.kohler@gmx.at>
2010-09-24 19:49:29 +0200

    newsletter

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,8 +1,10 @@
 class NewsletterMailer < ActionMailer::Base
+	
   def newsletter(recipient, page)
 			@page = page
 	    mail(:from => User.find(page.website_id).email,
-	 				  # headers["Reply-to"] =  User.find(page.website_id).email,
+	 				 # :headers['X-No-Spam' => 'True'],
+					 :reply_to => User.find(page.website_id).email,
 					 :to => recipient.email,
 	         :subject => page.name)
 	 end
