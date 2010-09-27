1b8cec61988757e0eb8cfcc8545ddf532ed3c5f2
Thomas Kohler <thomas.kohler@gmx.at>
2010-09-28 00:13:02 +0200

    codemirror changes for css and js highlithing

--- app/controllers/theme_uploads_controller.rb
+++ app/controllers/theme_uploads_controller.rb
@@ -30,6 +30,57 @@ class ThemeUploadsController < ApplicationController
 		ThemeUpload.destroy_all(:id => params[:theme_upload_ids])
 		redirect_to theme_uploads_path(:theme_id => params[:theme_id])
 	end
+	
+	def new
+		@theme_upload = ThemeUpload.new
+		@theme_id = params[:theme_id]
+		@theme = Theme.find(@theme_id)
+		render :layout => 'popup'#'themes'
+	end
+	
+	def create
+		theme_upload = ThemeUpload.new
+		the_filename = ''
+		
+		if params[:theme_upload][:theme_file_file_name].include?('.')
+			the_filename = params[:theme_upload][:theme_file_file_name].split('.').first
+		else
+			the_filename = params[:theme_upload][:theme_file_file_name]
+		end
+		
+		exists = false
+		ThemeUpload.find_all_by_theme_id(params[:theme_upload][:theme_id]).each do |file|
+			if the_filename.to_s.eql?(file.theme_file_file_name.split('.').first)
+			 	exists = true
+			end
+		end
+		
+		unless exists
+			theme_upload.theme_id = params[:theme_upload][:theme_id]
+			theme_upload.theme_file = File.new(the_filename, "w+")
+			theme_upload.theme_file_content_type = params[:theme_upload][:theme_file_content_type]
+			if params[:theme_upload][:theme_file_content_type].to_s.eql?('application/javascriptapplication/x-javascript')
+				theme_upload.theme_file_file_name = theme_upload.theme_file_file_name << '.js'
+			elsif params[:theme_upload][:theme_file_content_type].to_s.eql?('text/css')
+				theme_upload.theme_file_file_name = theme_upload.theme_file_file_name << '.css'
+			end
+			if theme_upload.save
+				redirect_to upload_files_eiditing_path(:theme_id => theme_upload.theme_id, :name => theme_upload.theme_file.original_filename)
+			else
+				redirect_to edit_theme_path(theme_path.theme_id)
+			end
+		else
+			redirect_to edit_theme_path(params[:theme_upload][:theme_id]) 
+		end
+	end
+	
+	def rename
+		
+	end
+	
+	def destroy
+		
+	end
 
 	private 
 
